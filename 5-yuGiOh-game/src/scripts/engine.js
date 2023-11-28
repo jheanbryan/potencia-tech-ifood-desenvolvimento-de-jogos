832eb5f8a73f74f48e1cc6594d453778254b5430
Jhean Bryan <93744970+jheanbryan@users.noreply.github.com>
2023-11-28 17:44:35 -0400

    updates

---
+++ 5-yuGiOh-game/src/scripts/engine.js
@@ -0,0 +1,27 @@
+const states = {
+    score:{
+        playerScore: 0,
+        computerScore: 0,
+        scoreBox: document.getElementById('score-ponts')
+    },
+    cardsSprites:{
+        avatar: document.getElementById('card-image'),
+        name: document.getElementById('card-name'),
+        type: document.getElementById('card-type')
+    },
+    fieldCards:{
+        player: document.getElementById('player-field-card'),
+        computer: document.getElementById('computer-field-card'),
+    },
+    button: document.getElementById('next-duel')
+};
+
+
+
+function init(){
+
+
+}
+
+
+init();
